c452c25862b56cf99a9e2688d1ec6914f72ddbaf
pciturri <pciturri@gfz-potsdam.de>
2024-03-06 17:02:34 +0100

    build: change environment to be built with mamba rather than conda

--- build_conda.sh
+++ build_conda.sh
@@ -1,5 +1,5 @@
 
-conda create -n float_italy -y
-conda activate float_italy
-conda install -c conda-forge floatcsep==0.1.4 -y
+mamba create -n float_italy -y
+mamba activate float_italy
+mamba install -c conda-forge floatcsep==0.1.4 -y
 pip install -r extra_reqs.txt
