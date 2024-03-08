1746f730ee653c49ed529ce50bb733fc269ebdd3
pciturri <pciturri@gfz-potsdam.de>
2024-03-08 16:18:34 +0100

    updated build instructions

--- build_conda.sh
+++ build_conda.sh
@@ -1,5 +1,3 @@
 
 mamba create -n float_italy -y
-mamba activate float_italy
-mamba install -c conda-forge floatcsep==0.1.4 -y
-pip install -r extra_reqs.txt
+mamba activate float_italy && mamba install -c conda-forge floatcsep==0.1.4 -y && pip install -r extra_reqs.txt
