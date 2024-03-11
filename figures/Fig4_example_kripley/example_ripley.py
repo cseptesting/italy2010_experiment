33918a5d5385631d012cdaca771b59d81baf40bc
pciturri <pciturri@gfz-potsdam.de>
2024-03-11 17:10:30 +0100

    ft: added figure gen code and inkscape ft: added final src evaluations code

---
+++ figures/Fig4_example_kripley/example_ripley.py
@@ -0,0 +1,239 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import numpy
+
+import h5py
+import matplotlib.pyplot as plt
+import numpy as np
+import floatcsep.utils
+import csep
+import floatcsep
+from floatcsep import experiment
+import seaborn as sns
+import cartopy
+import rioxarray as rxr
+sns.set_style("darkgrid", {"axes.facecolor": ".9", 'font.family': 'Ubuntu'})
+plt.rcParams.update({
+    'xtick.bottom': True,
+    'ytick.left': True,
+    'xtick.labelsize': 10,
+    'ytick.labelsize': 10,
+    'legend.fontsize': 9})
+
+
+def read_hdf5(fname):
+
+    with h5py.File(fname, 'r') as db:
+        parsed_results = dict.fromkeys(db.keys())
+        for key, val in db.items():
+            if key != 'name':
+                parsed_results[key] = val[:]
+            else:
+                parsed_results[key] = val
+
+    return parsed_results
+
+
+def plot_forecasts_sims():
+
+    cfg = '../../src/total/config.yml'
+    exp = experiment.Experiment.from_yml(cfg)
+    exp.set_tasks()
+    exp.stage_models()
+
+    # CATALOG
+    prop_forecast_a = {'grid_labels': True,
+                       'borders': False,
+                       'region_border': False,
+                       'coastline': False,
+                       'feature_lw': 0.5,
+                       'cmap': 'magma',
+                       'alpha_exp': 0.3,
+                       'clim': [-6, -1],
+                       'basemap': None,
+                       'projection': cartopy.crs.Mercator()}
+
+    prop_forecast_b = {'grid_labels': True,
+                       'borders': False,
+                       'region_border': False,
+                       'coastline': False,
+                       'feature_lw': 0.5,
+                       'cmap': 'magma',
+                       'alpha_exp': 0.3,
+                       'clim': [-6, -1],
+                       'basemap': None,
+                       'projection': cartopy.crs.Mercator()}
+
+    cat_props = {'markersize': 12,
+                 'markercolor': 'red',
+                 'mag_scale': 1,
+                 'legend': False,
+                 'coastline': False,
+                 'basemap': None,
+                 'alpha': 0.5}
+
+    sim_props = {'markersize': 8,
+                 'markercolor': 'lightgrey',
+                 'mag_scale': 1,
+                 'coastline': False,
+                 'legend': False,
+                 'basemap': None,
+                 'alpha': 0.8}
+    #
+    cat = csep.load_catalog('../../catalogs/catalog_ml.json')
+    region = csep.regions.italy_csep_region()
+    cat.filter_spatial(region, in_place=True)
+    projection = cartopy.crs.Mercator()
+
+    counts = cat.spatial_counts()
+    A = np.zeros(region.idx_map.shape)
+    A[ A == 0] = np.nan
+    for i in range(A.shape[0]):
+        for j in range(A.shape[1]):
+            if not np.isnan(region.idx_map[i,j]):
+                A[i, j] = counts[int(region.idx_map[i,j])]
+
+    cat_ind = cat.get_spatial_idx()
+    Events = np.array([[i, j] for i, j in zip(cat.get_longitudes(), cat.get_latitudes())])
+
+    model_a = exp.get_model('HAZFX_BPT')
+    model_b = exp.get_model('TripleS-CSI')
+
+    tstring = floatcsep.utils.timewindow2str(exp.timewindows[0])
+    model_a.create_forecast(tstring)
+    model_b.create_forecast(tstring)
+
+    forecast_a = model_a.get_forecast(tstring)
+    forecast_b = model_b.get_forecast(tstring)
+
+    n_sim = 1
+    numpy.random.seed(25)   # 4, 25
+
+    #################
+    # Forecast A
+    #################
+
+    rates_eqk = forecast_a.spatial_counts()[cat_ind]
+    observed_data = cat.spatial_counts()
+    n_obs = numpy.sum(observed_data)
+    n_fore = numpy.sum(forecast_a.spatial_counts())
+    scale = n_obs / n_fore
+    expected_forecast_count = int(n_obs)
+    num_events_to_simulate = expected_forecast_count
+    sampling_weights = numpy.cumsum(forecast_a.spatial_counts().ravel()) / \
+                       numpy.sum(forecast_a.spatial_counts())
+    sim_fore = numpy.zeros(sampling_weights.shape)
+    sim = csep.core.poisson_evaluations._simulate_catalog(num_events_to_simulate, sampling_weights, sim_fore)
+    ind_sim = np.where(sim != 0)[0]
+    events_sim = np.array([forecast_a.get_longitudes()[ind_sim], forecast_a.get_latitudes()[ind_sim]]).T
+    rates_sim = forecast_a.data[ind_sim]
+    tuple = [ (1, 1, i[1], i[0], 5,10) for i in events_sim]
+    sim_cat = csep.catalogs.CSEPCatalog(data=tuple)
+    sim_cat.region=region
+
+    raster_fn = 'basemap_3857.tif'
+    rds = rxr.open_rasterio(raster_fn)
+    os.makedirs('forecasts', exist_ok=True)
+
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(111, projection=projection)
+    rds.plot.imshow(ax=ax, transform=projection, add_labels=False)
+    Ax = forecast_a.plot(ax=ax, plot_args=prop_forecast_a)
+    Ax = cat.plot(ax=Ax, plot_args=cat_props)
+    sim_props['title'] = forecast_a.name
+    sim_cat.plot(ax=Ax, plot_args=sim_props, show=True, extent=region.get_bbox())
+    pts = region.tight_bbox()
+    ax.plot(pts[:, 0], pts[:, 1], lw=1, color='black',
+            transform=cartopy.crs.PlateCarree())
+    plt.savefig('forecast_a.png', dpi=300)
+    plt.show()
+
+    #
+    numpy.random.seed(43)   # 27, 43
+    ##################
+    ### Forecast B
+    ##################
+    rates_eqk = forecast_b.spatial_counts()[cat_ind]
+    observed_data = cat.spatial_counts()
+    n_obs = numpy.sum(observed_data)
+    n_fore = numpy.sum(forecast_b.spatial_counts())
+    scale = n_obs / n_fore
+    expected_forecast_count = int(n_obs)
+    num_events_to_simulate = expected_forecast_count
+    sampling_weights = numpy.cumsum(forecast_b.spatial_counts().ravel()) / \
+                       numpy.sum(forecast_b.spatial_counts())
+    sim_fore = numpy.zeros(sampling_weights.shape)
+    sim = csep.core.poisson_evaluations._simulate_catalog(num_events_to_simulate, sampling_weights, sim_fore)
+    ind_sim = np.where(sim != 0)[0]
+    events_sim = np.array([forecast_b.get_longitudes()[ind_sim], forecast_b.get_latitudes()[ind_sim]]).T
+    rates_sim = forecast_b.data[ind_sim]
+    tuple = [(1, 1, i[1], i[0], 5,10) for i in events_sim]
+    sim_cat = csep.catalogs.CSEPCatalog(data=tuple)
+    sim_cat.region = region
+
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(111, projection=projection)
+    rds.plot.imshow(ax=ax, transform=projection, add_labels=False)
+    Ax = forecast_b.plot(ax=ax, plot_args=prop_forecast_b)
+    Ax = cat.plot(ax=Ax, plot_args=cat_props)
+    sim_props['title'] = forecast_b.name
+    sim_cat.plot(ax=Ax, plot_args=sim_props, show=True, extent=region.get_bbox())
+    pts = region.tight_bbox()
+    ax.plot(pts[:, 0], pts[:, 1], lw=1, color='black',
+            transform=cartopy.crs.PlateCarree())
+    plt.savefig('forecast_b.png', dpi=300)
+    plt.show()
+
+
+def plot_l_funcs(alpha=0.05, figsize=(6, 4)):
+
+    ripley_path = os.path.join('../../src', 'ripley')
+
+    results_hazfx = read_hdf5(os.path.join(ripley_path,
+                                           'results',
+                                           f'K_HAZFX_BPT.hdf5'))
+    results_hazfx['name'] = 'HAZFX-BPT'
+    results_triples = read_hdf5(os.path.join(ripley_path,
+                                           'results',
+                                           f'K_TripleS-CSI.hdf5'))
+    results_triples['name'] = 'TripleS-CSI'
+    fig, axs = plt.subplots(1, 2, figsize=(10, 4), constrained_layout=True,
+                            sharey=True)
+    for ax, result in zip(axs, [results_hazfx, results_triples]):
+        name = result.get('name', 'None')
+        r = result['r']
+        l_sims = result['l_sims'][::7]
+        l_cat = result['l_cat']
+        L_avg = result['l_mean']
+        L_up = result[f'l_{1-alpha/2:.2f}']
+        L_down = result[f'l_{alpha/2:.2f}']
+        L_over = result[f'l_over_{alpha:.2f}']
+
+        for i in l_sims:
+            sns.lineplot(ax=ax, x=r, y=i, lw=0.05, color='black')
+        sns.lineplot(ax=ax, x=r, y=l_cat, color='r', label='Observed catalog',
+                     legend=False)
+        sns.lineplot(ax=ax, x=r, y=L_avg, label='Sim. average',
+                     legend=False)
+        ax.fill_between(r, L_down, L_up, color='gray', alpha=0.4,
+                        label=r'Sim. envelope ($\gamma=%.2f$)' % (1 - alpha))
+        ax.fill_between(r, L_up, l_cat, where=l_cat >= L_up,
+                        color='red', alpha=0.3, label=r'Envelope distance')
+
+        ax.set_xlim([None, np.max(r)])
+        ax.set_xlabel(r"Bandwidth $r~~\mathrm{[km]}$", fontsize=15)
+        ax.set_ylim([0, 950])
+        ax.set_title(name, fontsize=18, loc='left')
+    axs[0].set_ylabel(r"$L(r) = \sqrt{\frac{K(r)}{\pi}}$", fontsize=18)
+    axs[0].legend(loc=2, fontsize=12)
+
+    plt.savefig(f'L_models.png', dpi=300, facecolor=(0, 0, 0, 0))
+    plt.show()
+
+
+if __name__ == '__main__':
+    plot_forecasts_sims()
+    plot_l_funcs()
+
+
---
+++ figures/Fig8_sequential_IG/fig_seq_iggain.py
@@ -0,0 +1,77 @@
+import numpy
+import seaborn
+import matplotlib.pyplot as plt
+import matplotlib.ticker as tick
+from matplotlib.lines import Line2D
+from datetime import datetime
+
+import csep
+from floatcsep import experiment
+from floatcsep.utils import plot_sequential_likelihood
+seaborn.set_style("white", {"axes.facecolor": ".9", 'font.family': 'Ubuntu'})
+plt.rcParams.update({
+    'axes.titleweight': 'bold',
+    'xtick.bottom': True, 'axes.labelweight': 'bold',
+    'xtick.labelsize': 8, 'ytick.labelsize': 8,
+    'legend.fontsize': 9})
+
+
+def plot_seq_ig(experiment, figpath=None):
+
+    catalog = experiment.catalog
+    ig_seq = [i for i in experiment.tests if i.name == 'Sequential_IG'][0]
+    ig_results = experiment.read_results(ig_seq, experiment.timewindows[-1])
+    rank = numpy.argsort([i.observed_statistic[-1] for i in ig_results])[::-1]
+    ig_results = [ig_results[i] for i in rank]
+    colors = ['steelblue', 'darkturquoise',
+              'darkgreen', 'limegreen',
+              'xkcd:neon yellow', 'gold', 'xkcd:yellowish orange',
+              'xkcd:raw umber', 'xkcd:brown', 'xkcd:cement',
+              'xkcd:fuchsia', 'xkcd:pinkish purple', 'xkcd:purple',
+              'xkcd:indigo', 'xkcd:shocking pink',
+              'xkcd:medium pink', 'red', 'xkcd:darkish red', 'xkcd:black']
+
+    linestyles = ['--', '-',  '-.', '-.', ':',  '-', '--', '-',  '-', '--',  '-',
+                  '-', '-', ':',  '-', '-', '-', '-', '-']
+    startyear = experiment.timewindows[0][0]
+    endyears = [j[1] for j in experiment.timewindows]
+    years = [startyear] + endyears
+    fig, ax = plt.subplots(figsize=(7, 3.7))
+    for i, result in enumerate(ig_results):
+        data = [0] + result.observed_statistic
+        ax.plot(years, data, color=colors[i],
+                linewidth=1.2, linestyle=linestyles[i],
+                marker='o',
+                markersize=2,
+                label=result.sim_name)
+        ax.set_ylabel('Information Gain\n(respect to MPS04)')
+        ax.set_xlabel('Year', fontsize=10)
+        ax.set_xlim([startyear, None])
+        ax.grid(True)
+    ax.legend(loc=(1.07, 0), fontsize=7)
+    ax.set_ylim([-20, 16])
+
+    plt.show()
+
+    ax2 = ax.twinx()
+    mag = catalog.data['magnitude']
+    time = [datetime.fromtimestamp(i / 1000.) for i in
+            catalog.data['origin_time']]
+    ax2.scatter(time, mag, marker='o', s=2**((mag-4) * 4),
+                edgecolor='gray', color='r', alpha=0.2)
+    ax2.set_ylim([3., 6.2])
+    ax2.set_ylabel('$M_w$', fontsize=14, loc='top', rotation='horizontal')
+    ax2.set_yticks([5., 5.5, 6.0])
+    ax2.yaxis.set_label_coords(1.06, 1.1)
+    fig.tight_layout()
+    if figpath:
+        plt.savefig(figpath, dpi=300)
+    plt.show()
+
+
+if __name__ == '__main__':
+    cfg = '../../src/sequential/config.yml'
+    experiment = experiment.Experiment.from_yml(cfg)
+    experiment.set_tasks()
+    cl_figpath = 'Sequential_IG.png'
+    plot_seq_ig(experiment, cl_figpath)
